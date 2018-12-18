428715337e02143785b1f7bb9b6b2bd660188be1
Alligatooo <rittner.benni@t-online.de>
2018-12-18 19:29:08 +0100

    Outsourcing of the handleFunctions

--- app.js
+++ app.js
@@ -34,81 +34,24 @@ client.on('message', async message => {
     switch (command) {
 
         case 'HELP':       //Sends the user all commands
-            message.channel.send(`${message.author} "Needs to be done"`);
+            handleHelp(message, args);
             break;
 
         case 'ADD': //Adds a new role to a user
-            if (args[0] === "" || args[0] === undefined) {
-                message.channel.send("Missing argument. Correct command: mi!add [toAdd]");
-                return;
-            }
-
-            if (args[0].startsWith("@")) args[0] = args[0].substring(1, args[0].length);
-
-            roleToChange = message.channel.guild.roles.find(val => val.name === args[0]); //Searches for the role
-
-            if (roleToChange === null) { //Couldn't find a role
-                message.channel.send("Couldn't find a role with the name " + args[0]);
-                return;
-            }
-
-            //Found a role with the given name and adds it to the member
-            message.member.addRole(roleToChange.id);
-            message.channel.send("You now have access to the channel " + roleToChange.name);
-            return;
+            handleAdd(message, args);
+            break;
 
         case 'REM': //removing a role
-            //Checking if argument is empty
-            if (args[0] === "" || args[0] === undefined) {
-                message.channel.send("Missing argument. Correct command mi!rem [toRemove]");
-            }
-
-            //Removing '@'
-            if (args[0].startsWith("@")) args[0] = args[0].substring(1, args[0].length);
-
-            //Checks if the user has the given role, so he could remove it
-            roleToChange = message.member.roles.find(val => val.name === args[0]);
-
-            //The user doesn't have the role
-            if (roleToChange === null) {
-                message.channel.send("Couldn't find a role with that name " + args[0]);
-                return;
-            }
-
-            //Found a role with the given name and removes it from the member
-            message.member.removeRole(roleToChange.id);
-            message.channel.send("The role is now removed!");
+            handleRemove(message, args);
             break;
 
 
         case 'LISTREMOVE': //Lists all removable roles for this user
-
-            //Removes specific roles like admin, everyone
-            let removableRoles = message.member.roles.filter(role => !noChannelRoles.includes(role.name,0)).array();
-
-            //Checks if there is a role the user can remove
-            if (removableRoles.length === 0) {
-                message.channel.send("There is no role able to be removed!");
-            } else
-                message.channel.send("Removable roles: " + removableRoles.toString());
+            handleListRemove(message, args);
             break;
 
         case 'LISTADD': //Sends all available roles
-
-            //Removes specific roles like admin
-            let availableRoles = message.channel.guild.roles.filter(val => !noChannelRoles.includes(val.name, 0));
-
-            //Removes roles the member already has
-            availableRoles = availableRoles.filter(val => !message.member.roles.array().includes(val));
-
-            //availableRoles = availableRoles.map(x => x.name);
-
-            if (availableRoles.array().length === 0) {
-                message.channel.send("There are no available roles left!");
-                return;
-            }
-
-            message.channel.send("Available roles: " + availableRoles.array().toString());
+            handleListAdd(message, args);
             return;
         default:            //Cannot identify any command
             message.channel.send("Wrong command! \r\nCheck \"mi!help\" for more information");
@@ -120,6 +63,84 @@ client.on('error', error => {
     console.log(error.name);
     console.log(error.message);
 });
+
+
+async function handleHelp(message, args) {
+    message.channel.send(`${message.author} "Needs to be done"`);
+}
+
+async function handleAdd(message, args) {
+    if (args[0] === "" || args[0] === undefined) {
+        message.channel.send("Missing argument. Correct command: mi!add [toAdd]");
+        return;
+    }
+
+    if (args[0].startsWith("@")) args[0] = args[0].substring(1, args[0].length);
+
+    roleToChange = message.channel.guild.roles.find(val => val.name === args[0]); //Searches for the role
+
+    if (roleToChange === null) { //Couldn't find a role
+        message.channel.send("Couldn't find a role with the name " + args[0]);
+        return;
+    }
+
+    //Found a role with the given name and adds it to the member
+    message.member.addRole(roleToChange.id);
+    message.channel.send("You now have access to the channel " + roleToChange.name);
+}
+
+async function handleRemove(message, args) {
+
+    //Checking if argument is empty
+    if (args[0] === "" || args[0] === undefined) {
+        message.channel.send("Missing argument. Correct command mi!rem [toRemove]");
+    }
+
+    //Removing '@'
+    if (args[0].startsWith("@")) args[0] = args[0].substring(1, args[0].length);
+
+    //Checks if the user has the given role, so he could remove it
+    roleToChange = message.member.roles.find(val => val.name === args[0]);
+
+    //The user doesn't have the role
+    if (roleToChange === null) {
+        message.channel.send("Couldn't find a role with that name " + args[0]);
+        return;
+    }
+
+    //Found a role with the given name and removes it from the member
+    message.member.removeRole(roleToChange.id);
+    message.channel.send("The role is now removed!");
+}
+
+async function handleListRemove(message, args) {
+    //Removes specific roles like admin, everyone
+    let removableRoles = message.member.roles.filter(role => !noChannelRoles.includes(role.name, 0)).array();
+
+    //Checks if there is a role the user can remove
+    if (removableRoles.length === 0) {
+        message.channel.send("There is no role able to be removed!");
+    } else
+        message.channel.send("Removable roles: " + removableRoles.toString());
+}
+
+async function handleListAdd(message, args) {
+    //Removes specific roles like admin
+    let availableRoles = message.channel.guild.roles.filter(val => !noChannelRoles.includes(val.name, 0));
+
+    //Removes roles the member already has
+    availableRoles = availableRoles.filter(val => !message.member.roles.array().includes(val));
+
+    //availableRoles = availableRoles.map(x => x.name);
+
+    if (availableRoles.array().length === 0) {
+        message.channel.send("There are no available roles left!");
+        return;
+    }
+
+    message.channel.send("Available roles: " + availableRoles.array().toString());
+}
+
 
 function sendUserMessage(userId, message) {
     var s = client.users.get(userId);
